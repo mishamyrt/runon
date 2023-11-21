10299ec4ca87e9bc9ae613bd7b33185bdadcdabe
mishamyrt <misha@myrt.co>
2023-11-22 01:20:24 +0300

    feat: use bash script for daemonizing

--- Sources/RunIf.swift
+++ Sources/RunIf.swift
@@ -1,46 +1,31 @@
 import ArgumentParser
-import Cocoa
-import Foundation
 
-struct Options: ParsableArguments {
+@main
+struct RunIf: ParsableCommand {
+    static var configuration = CommandConfiguration(
+        commandName: "runif-daemon",
+        abstract: "A utility for automating actions on system events.",
+        version: "1.0.0"
+    )
+
     @Option(
         name: [.customLong("config"), .customShort("c")],
         help: "Configuration file path."
     )
     var configPath: String?
-}
 
-@main
-struct RunIf: ParsableCommand {
-    static var configuration = CommandConfiguration(
-        commandName: "runif",
-        abstract: "A utility for automating actions on system events.",
-        version: "1.0.0",
-        subcommands: [Run.self],
-        defaultSubcommand: Run.self
-    )
-    static let configError = ValidationError("Can't open config file.")
-}
-
-extension RunIf {
-    struct Run: ParsableCommand {
-        static var configuration =
-            CommandConfiguration(abstract: "Run observer in connected mode.")
-
-        @OptionGroup
-        var options: Options
-
-        mutating func run() throws {
-            let config = ConfigLoader.read(handlersOf: options.configPath)
-            if config == nil {
-                throw configError
-            }
-            let runner = CommandRunner(with: config!)
-            let observer = EventObserver(sources: [
-                ScreenSource()
-            ])
-            observer.listener = runner
-            observer.runLoop()
+    mutating func run() throws {
+        let config = ConfigLoader.read(handlersOf: configPath)
+        if config == nil {
+            throw ValidationError("Can't open config file.")
         }
+        let runner = CommandRunner(with: config!)
+        let sources: [EventSource] = [
+            ScreenSource()
+        ]
+        let observer = EventObserver(sources: sources)
+        observer.listener = runner
+        print("starting with \(sources.count) event sources")
+        observer.runLoop()
     }
 }
